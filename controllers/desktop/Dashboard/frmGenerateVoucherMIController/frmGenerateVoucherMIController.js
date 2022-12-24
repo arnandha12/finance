8888186cfb850e507df19e9101a6ff3fcda3e35c
DESKTOP-U4GIPNR\ADMIN <>
2022-12-24 23:53:02 +0530

    Functional implementation

--- controllers/desktop/Dashboard/frmGenerateVoucherMIController/frmGenerateVoucherMIController.js
+++ controllers/desktop/Dashboard/frmGenerateVoucherMIController/frmGenerateVoucherMIController.js
@@ -1,3 +1,46 @@
 define([],function(){
-  
+  let voucherStatus = {"CREATED" : "CREATED",
+                       "Redeemed" : "REDEEMED",
+                       "Unutilised" : "UNUTILISED",
+                       "Cancelled" : "CANCELLED",
+                       "Expired" : "EXPIRED",
+                       "All" : "All"
+                      };
+  let selectedStatus = "",startDate = "",endDate = "";
+  return {
+    onPreShow: function() {
+      var self = this;
+      selectedStatus = "";
+      startDate = "";
+      this.view.flxSegStatus.isVisible = false;
+      this.view.txtSearchVoucher.text = "";
+      this.view.CalenderStartDate.text = "";
+      this.view.CalenderEndDate.text = "";
+      this.view.btnSelectStatus.onClick = function() {
+        if(self.view.flxSegStatus.isVisible) {
+          self.view.flxSegStatus.isVisible = false;
+        } else {
+          self.view.flxSegStatus.isVisible = true;  
+        }
+      };
+      this.view.segStatus.onRowClick = function(eventId) {
+        kony.print("event :: "+JSON.stringify(eventId.selectedRowItems[0]));
+        selectedStatus = voucherStatus[eventId.selectedRowItems[0].lblUserName];
+        kony.print("selectedStatus :: "+selectedStatus);
+        self.view.flxSegStatus.isVisible = false;
+      };
+      this.view.CalenderStartDate.onSelection = function(eventId) {
+        let day = self.view.CalenderStartDate.day;
+        let month = self.view.CalenderStartDate.month;
+        let year = self.view.CalenderStartDate.year;
+        startDate = year+"-"+month+"-"+day;
+      };
+      this.view.CalenderEndDate.onSelection = function(eventId) {
+        let day = self.view.CalenderEndDate.day;
+        let month = self.view.CalenderEndDate.month;
+        let year = self.view.CalenderEndDate.year;
+        endDate = year+"-"+month+"-"+day;
+      };
+    }
+  };
 });
